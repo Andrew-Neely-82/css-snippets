dcfeccc0b27ddc429c3112ff1bc920dc4be23600
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-06-26 16:07:31 -0500

    CodeBlock component created with prop for reusability

---
+++ src/components/codeBlock/index.js
@@ -0,0 +1,5 @@
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import CheckIcon from "@mui/icons-material/Check";
+
+export const Copy = ContentCopyIcon;
+export const Check = CheckIcon;
