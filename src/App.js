9606522151a5b9b2bbc37c63c5bc9cc079e16024
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-06-26 15:32:00 -0500

    project basics setup complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Navbar, Footer } from "./components/export";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./styling/style.scss";
+
+const Navbar = () => {
+  return (
+    <div className="Navbar__">
+      <div className="Navbar__title">CSS Snippets</div>
+      <div className="Navbar__links"></div>
+    </div>
+  );
+};
+export default Navbar;
