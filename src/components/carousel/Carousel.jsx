254af71c807877ec5edc4e0bfc8b4fecaea3b42f
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-06-27 13:04:40 -0500

    updated Carousel jsx to map paramters automatically for CodeContainer comp

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,18 +1,27 @@
 import { CodeContainer } from "../export";
+import { examples, code } from "../../data/data";
 import "./styling/style.scss";
 
 const Carousel = () => {
-  const title = "Heading test";
-  const test = "This is a test of passing data";
+  const exampleProps = examples.map((item) => ({
+    exampleTitle: item.title,
+    example: item.example,
+  }));
+
+  const codeProps = code.map((item) => ({
+    blockTitle: item.title,
+    code: item.code,
+  }));
 
   return (
     <div className="Carousel__">
       <div className="Carousel__wrapper">
-        <CodeContainer exampleTitle={title} example={test} blockTitle={title} code={test} />
-        <CodeContainer exampleTitle={title} example={test} blockTitle={title} code={test} />
-        <CodeContainer exampleTitle={title} example={test} blockTitle={title} code={test} />
+        {exampleProps.map((example, index) => (
+          <CodeContainer {...example} {...codeProps[index]} key={example.exampleTitle} />
+        ))}
       </div>
     </div>
   );
 };
+
 export default Carousel;
---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+export const examples = [
+  {
+    title: "example 1",
+    example: "example 1",
+  },
+  {
+    title: "example 2",
+    example: "example 2",
+  },
+  {
+    title: "example 2",
+    example: "example 2",
+  },
+];
+
+export const code = [
+  {
+    title: "code 1",
+    code: "code 1",
+  },
+  {
+    title: "code 2",
+    code: "code 2",
+  },
+  {
+    title: "code 3",
+    code: "code 3",
+  },
+];
